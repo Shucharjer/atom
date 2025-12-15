c620d08f9acd6ff8899e9ba4da9a2cdba3e40b22
Shucharjer <Shucharjer@outlook.com>
2025-12-15 17:38:25 +0800

    update test case of proton

--- proton/test/include/example_application.hpp
+++ proton/test/include/example_application.hpp
@@ -3,24 +3,13 @@
 #include <memory_resource>
 #include <thread>
 #include <tuple>
-#include <utility>
 #include <vector>
-// #include <neutron/thread_pool.hpp>
-#include <neutron/parallel.hpp>
+#include <exec/static_thread_pool.hpp>
 #include <proton/registry.hpp>
 #include "neutron/execution.hpp"
 #include "proton/command_buffer.hpp"
 #include "proton/stage.hpp"
 #include "proton/world.hpp"
-
-template <typename Alloc>
-struct context_maker {
-    template <typename Al = Alloc>
-    constexpr auto operator()(const Al& alloc = {})
-        -> proton::command_buffer<Alloc> {
-        return proton::command_buffer<Alloc>{ alloc };
-    }
-};
 
 class myapp {
 public:
@@ -29,6 +18,7 @@ public:
     template <auto World> // single world
     void run() {
         using namespace neutron;
+        using namespace neutron::execution;
         using namespace proton;
         using enum stage;
 
@@ -42,23 +32,33 @@ public:
 
         // create execution environment
 
-        const auto concurrency = std::thread::hardware_concurrency();
-        auto thread_pool =
-            static_context_thread_pool<context_maker, allocator_t>{
-                concurrency
-            };
-        execution::scheduler auto sch = thread_pool.get_scheduler();
-        execution::sender auto sndr   = sch.schedule();
-        // using command_buffer   = command_buffer<decltype(alloc)>;
-        // auto command_buffers   = std::vector<
-        //       command_buffer, rebind_alloc_t<decltype(alloc),
-        //       command_buffer>>{
-        //     32
-        // };
+        auto thread_pool   = exec::static_thread_pool{};
+        scheduler auto sch = thread_pool.get_scheduler();
+
+        // make command buffers
+
+        const auto concurrency = thread_pool.available_parallelism();
+        std::vector<command_buffer<allocator_t>> command_buffers(concurrency);
+
+        // make worlds
 
         auto worlds = make_worlds<World>(alloc);
-        // call_startup(worlds, sch);
-        // call_update(worlds, sch);
-        // call<shutdown>(worlds, sch);
+
+        // startup
+        call<pre_startup>(sch, command_buffers, worlds);
+        call<startup>(sch, command_buffers, worlds);
+        call<post_startup>(sch, command_buffers, worlds);
+
+        // update
+
+        constexpr auto total_frames = 4;
+        for (auto frame = 0; frame < total_frames; ++frame) {
+            call<pre_update>(sch, command_buffers, worlds);
+            call<update>(sch, command_buffers, worlds);
+            call<post_update>(sch, command_buffers, worlds);
+        }
+
+        // shutdown
+        call<shutdown>(sch, command_buffers, worlds);
     }
 };
--- proton/test/src/commands.cpp
+++ proton/test/src/commands.cpp
@@ -1,18 +1,68 @@
+#include <cstddef>
+#include <mutex>
+#include <sstream>
+#include <thread>
+#include <exec/static_thread_pool.hpp>
+#include <neutron/execution.hpp>
+#include <neutron/print.hpp>
 #include <proton/args/common/commands.hpp>
 #include <proton/command_buffer.hpp>
-#include <neutron/print.hpp>
+#include <proton/proton.hpp>
+#include <proton/registry.hpp>
+#include <proton/stage.hpp>
+#include <proton/system.hpp>
+#include <proton/world.hpp>
 
 using namespace neutron;
+using namespace execution;
 using namespace proton;
 
+void task1(commands);
+void task2(commands);
+void task3(commands);
+
+using enum stage;
+constexpr auto world = world_desc | add_system<update, &task1> |
+                       add_system<update, &task2> |
+                       add_system<update, &task3, after<task2>>;
+
 int main() {
-    command_buffer<> command_buffer;
-    commands cmds{ command_buffer };
+    exec::static_thread_pool pool;
+    std::vector<command_buffer<>> cbs(std::thread::hardware_concurrency());
+    scheduler auto sch = pool.get_scheduler();
 
-    auto entity = cmds.spawn();
-    println("entity {}", entity);
+    auto worlds = make_worlds<::world>();
 
-    command_buffer.reset();
+    for (auto frame = 0; frame < 4; ++frame) {
+        call<update>(sch, cbs, worlds);
+    }
 
     return 0;
+}
+
+std::mutex mutex;
+
+void task1(commands cmds) {
+    const auto tid = std::this_thread::get_id();
+    std::ostringstream oss;
+    oss << "tid: " << tid << ", task1, cb: " << static_cast<void*>(&cmds)
+        << '\n';
+    std::lock_guard guard{ mutex };
+    std::cout << oss.str();
+}
+void task2(commands cmds) {
+    const auto tid = std::this_thread::get_id();
+    std::ostringstream oss;
+    oss << "tid: " << tid << ", task2, cb: " << static_cast<void*>(&cmds)
+        << '\n';
+    std::lock_guard guard{ mutex };
+    std::cout << oss.str();
+}
+void task3(commands cmds) {
+    const auto tid = std::this_thread::get_id();
+    std::ostringstream oss;
+    oss << "tid: " << tid << ", task3, cb: " << static_cast<void*>(&cmds)
+        << '\n';
+    std::lock_guard guard{ mutex };
+    std::cout << oss.str();
 }
